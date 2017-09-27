6840cf44b381efb75e1555c0929ed6b0d69a52e5
Владимир Черноног <gyilfr246@gmail.com>
2017-09-27 19:07:15 +0300

    add comments, OpType enum just like a bonus:)

--- Server/src/server/Database.java
+++ Server/src/server/Database.java
@@ -3,13 +3,23 @@ package server;
 import java.sql.*;
 
 public class Database {
-	//format of url(jdbc:mysql://ip:port/database): "jdbc:mysql://localhost:3306/bank_users";
-	//default user: "root";
-	//default password: "root";
-
+	/*
+	 * Provides connection to DB
+	 */
 	private Connection connection;
+	
+	/*
+	 * Provides query execution
+	 */
 	private Statement statement;
 
+	/*
+	 * Class constructor, which connects to the DB bank_users with parameters:
+	 * @param url determines the driver, ip, port and DB name in given format:
+	 * jdbc:mysql://ip:port/database default example: "jdbc:mysql://localhost:3306/bank_users"
+	 * @param user user, that connects to DB. "root" by default
+	 * @param password users password. "root" by default
+	 */
 	public Database(String url, String user, String password) {
 		try {
 			connection = DriverManager.getConnection(url, user, password);
@@ -19,31 +29,49 @@ public class Database {
 		}
 	}
 
-	private Object execute(String type, String query) throws SQLException {
-		if (type.equals("insert") || type.equals("update")) {
+	/*
+	 * Method to execute specific query
+	 * @param type determines a type of query(insert||select||update)
+	 * @param query string to execute by DB
+	 */
+	private Object execute(OpType type, String query) throws SQLException {
+		if (type == OpType.INSERT || type == OpType.UPDATE) {
 			return statement.executeUpdate(query);
-		} else if (type.equals("select")) {
+		} else if (type == OpType.SELECT) {
 			return statement.executeQuery(query);
 		} else {
 			return null;
 		}
 	}
 
+	/*
+	 * Method to round balance of all users
+	 */
 	private void round() throws SQLException {
-		execute("update", "update users set balance = round(balance, 2);");
+		execute(OpType.UPDATE, "update users set balance = round(balance, 2);");
 	}
 
+	/*
+	 * Registrates new user
+	 * Generates a unique user_id
+	 * @param pin 4-digit number(password for new user)
+	 * @param userLogin login for new user, must be unique
+	 * @param birthDate birthdate for new user(can get by Date.valueOf(string in format yyyy-mm-dd))
+	 * @param secretQuestion question used to confirmation
+	 * @param secretAnswer answer used to confirmation
+	 * @return false because of error
+	 */
 	public boolean registryNewUser(int pin, String userLogin, Date birthDate,
 		String secretQuestion, String secretAnswer) {
 		try {
-			ResultSet maximum = (ResultSet) execute("select",
+			ResultSet maximum = (ResultSet) execute(OpType.SELECT,
 					"select max(user_id) from users;");
 			int max = 0;
 			while (maximum.next()) {
 				max = maximum.getInt(1);
 			}
 			maximum.close();
-			execute("insert", "insert into users values("
+			execute(OpType.INSERT, "insert into users values("
 					+ ++max + ","
 					+ " 0,"
 					+ pin + ","
@@ -60,9 +88,15 @@ public class Database {
 		}
 	}
 
+	/*
+	 * Method to authentificate user
+	 * @param pin users password
+	 * @param userLogin login of a user
+	 * @return boolean(true if confirmed, false if not)
+	 */
 	public boolean logIn(int pin, String userLogin) {
 		try {
-			ResultSet client = (ResultSet) execute("select",
+			ResultSet client = (ResultSet) execute(OpType.SELECT,
 					"select pin, user_login from users "
 					+ "where pin = " + pin + " and user_login = '" + userLogin + "';");
 			if (!client.first()) {
@@ -75,17 +109,24 @@ public class Database {
 		}
 	}
 
+	/*
+	 * Withdrawal money from account
+	 * @param userId account
+	 * @param money amount of money to withdraw
+	 * false if userId is unknown
+	 * false if not enought money on balance
+	 */
 	public boolean withdrawal(int userId, double money) {
 		if(money < 0) {
 			return false;
 		}
 		try {
-			ResultSet hasUser = (ResultSet) execute("select",
+			ResultSet hasUser = (ResultSet) execute(OpType.SELECT,
 					"select user_id from users where user_id = " + userId + ";" );
 			if (!hasUser.first()) {
 				return false;
 			}
-			ResultSet enoughtMoney = (ResultSet) execute("select",
+			ResultSet enoughtMoney = (ResultSet) execute(OpType.SELECT,
 					"select balance from users where user_id = "+ userId +";");
 			double balance = 0d;
 			while(enoughtMoney.next()) {
@@ -95,7 +136,7 @@ public class Database {
 			if(money > balance) {
 				return false;
 			}
-			execute("update", "update users set balance = balance - " + money
+			execute(OpType.UPDATE, "update users set balance = balance - " + money
 					+ " where user_id = " + userId + ";");
 			round();
 			return true;
@@ -105,18 +146,24 @@ public class Database {
 		}
 	}
 
+	/*
+	 * Refill money in account
+	 * @param userId account
+	 * @param money amount of money to refill
+	 * false if userId is unknown
+	 */
 	public boolean refill (int userId, double money) {
 		if(money < 0) {
 			return false;
 		}
 		try {
-			ResultSet hasUser = (ResultSet) execute("select",
+			ResultSet hasUser = (ResultSet) execute(OpType.INSERT,
 					"select user_id from users where user_id = " + userId + ";" );
 			if (!hasUser.first()) {
 				return false;
 			}
 			hasUser.close();
-			execute("update", "update users set balance = balance + " + money
+			execute(OpType.UPDATE, "update users set balance = balance + " + money
 					+ " where user_id = "+ userId + ";");
 			round();
 			return true;
@@ -126,25 +173,33 @@ public class Database {
 		}
 	}
 
+	/*
+	 * Transfer money from one account to another
+	 * @param userIdFrom account to withdraw
+	 * @param userIdTo account to refill
+	 * @param money amount of money to transfer
+	 * false if any of accounts unknown
+	 * false if userIdFrom have not enought money to transfer
+	 */
 	public boolean transfer(int userIdFrom, int userIdTo, double money) {
 		if(money < 0) {
 			return false;
 		}
 		try {
-			ResultSet hasUserFrom = (ResultSet) execute("select",
+			ResultSet hasUserFrom = (ResultSet) execute(OpType.SELECT,
 					"select user_id from users where user_id = " + userIdFrom + ";");
 			if (!hasUserFrom.first()) {
 				return false;
 			}
 			hasUserFrom.close();
-			ResultSet hasUserTo = (ResultSet) execute("select",
+			ResultSet hasUserTo = (ResultSet) execute(OpType.SELECT,
 					"select user_id from users where user_id = " + userIdTo + ";");
 			if (!hasUserTo.first()) {
 				return false;
 			}
 			hasUserTo.close();
 
-			ResultSet enoughtMoney =(ResultSet) execute("select",
+			ResultSet enoughtMoney =(ResultSet) execute(OpType.SELECT,
 					"select balance from users where user_id = " + userIdFrom + ";");
 			double balance = 0d;
 			while (enoughtMoney.next()) {
@@ -155,9 +210,9 @@ public class Database {
 				return false;
 			}
 
-			execute("update", "update users set balance = balance - " + money
+			execute(OpType.UPDATE, "update users set balance = balance - " + money
 					+ " where user_id = "+ userIdFrom + ";");
-			execute("update", "update users set balance = balance + " + money
+			execute(OpType.UPDATE, "update users set balance = balance + " + money
 					+ " where user_id = "+ userIdTo + ";");
 			round();
 			return true;
@@ -167,9 +222,14 @@ public class Database {
 		}
 	}
 
+	/*
+	 * Method to get user information
+	 * @param userId account identifier
+	 * @return object of class UserInfo
+	 */
 	public UserInfo getUserInfo(int userId) {
 		try {
-			ResultSet result = (ResultSet) execute("select",
+			ResultSet result = (ResultSet) execute(OpType.SELECT,
 					"select * from users where user_id = " + userId + ";");
 			UserInfo client = new UserInfo();
 			while(result.next()) {
---
+++ Server/src/server/OpType.java
@@ -0,0 +1,7 @@
+package server;
+
+public enum OpType {
+	SELECT,
+	INSERT,
+	UPDATE,
+}
