5f2c0db0be268a18deff6d364978cb1610d7cce8
Kondratenko Alexander <Lurcher13@github.com>
2017-09-29 08:30:53 +0200

    njk

--- Server/src/server/UserInfo.java
+++ Server/src/server/UserInfo.java
@@ -1,5 +1,25 @@
 package server;
 
-public class UserInfo {
+import java.sql.Date;
 
+public class UserInfo {
+	int userId;
+	double balance;
+	int pin;
+	String userLogin;
+	Date birthDate;
+	String secretQuestion;
+	String secretAnswer;
+	boolean status;
+	public UserInfo() {
+		this.userId = 0;
+		this.balance = 0;
+		this.pin = 0;
+		this.userLogin = "";
+		this.birthDate = Date.valueOf("1970-01-01");
+		this.secretQuestion = "";
+		this.secretAnswer = "";
+		this.status = false;
+	}
+	
 }
