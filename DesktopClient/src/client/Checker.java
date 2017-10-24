f5cfb54cac43429108e4c2864369a3fb67f9d726
Владимир Черноног <gyilfr246@gmail.com>
2017-10-24 09:35:39 +0300

    update login window add checker class

---
+++ DesktopClient/src/client/Checker.java
@@ -0,0 +1,15 @@
+package client;
+
+public static class Checker {
+  public boolean verifyLogin(String login) {
+    return false;
+  }
+
+  public boolean verifyPinCode(String pin) {
+    return false;
+  }
+
+  public boolean verifyDate(String date) {
+    return false;
+  }
+}
--- DesktopClient/src/wiev/LoginWindow.java
+++ DesktopClient/src/wiev/LoginWindow.java
@@ -53,14 +53,6 @@ public class LoginWindow {
 	public LoginWindow() {
 		initialize();
 	}
-	
-	public boolean checkLogin(String login) {
-		return false;
-	}
-	
-	public boolean checkPinCode(String pin) {
-		return false;
-	}
 
 	/**
 	 * Initialize the contents of the frame.
@@ -76,7 +68,7 @@ public class LoginWindow {
 		gridBagLayout.columnWeights = new double[]{1.0, 0.0, Double.MIN_VALUE};
 		gridBagLayout.rowWeights = new double[]{1.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		frmLogin.getContentPane().setLayout(gridBagLayout);
-		
+
 		JLabel lblYourLogin = new JLabel("Your login");
 		GridBagConstraints gbc_lblYourLogin = new GridBagConstraints();
 		gbc_lblYourLogin.gridwidth = 2;
@@ -84,7 +76,7 @@ public class LoginWindow {
 		gbc_lblYourLogin.gridx = 0;
 		gbc_lblYourLogin.gridy = 1;
 		frmLogin.getContentPane().add(lblYourLogin, gbc_lblYourLogin);
-		
+
 		loginInputField = new JTextField();
 		GridBagConstraints gbc_loginInputField = new GridBagConstraints();
 		gbc_loginInputField.fill = GridBagConstraints.HORIZONTAL;
@@ -94,7 +86,7 @@ public class LoginWindow {
 		gbc_loginInputField.gridy = 2;
 		frmLogin.getContentPane().add(loginInputField, gbc_loginInputField);
 		loginInputField.setColumns(10);
-		
+
 		lblYourPincode = new JLabel("Your PIN-code:");
 		GridBagConstraints gbc_lblYourPincode = new GridBagConstraints();
 		gbc_lblYourPincode.gridwidth = 2;
@@ -102,7 +94,7 @@ public class LoginWindow {
 		gbc_lblYourPincode.gridx = 0;
 		gbc_lblYourPincode.gridy = 3;
 		frmLogin.getContentPane().add(lblYourPincode, gbc_lblYourPincode);
-		
+
 		passwordInputField = new JPasswordField();
 		GridBagConstraints gbc_passwordInputField = new GridBagConstraints();
 		gbc_passwordInputField.fill = GridBagConstraints.HORIZONTAL;
@@ -110,7 +102,7 @@ public class LoginWindow {
 		gbc_passwordInputField.gridx = 0;
 		gbc_passwordInputField.gridy = 4;
 		frmLogin.getContentPane().add(passwordInputField, gbc_passwordInputField);
-		
+
 		btnForgot = new JButton("Forgot?");
 		btnForgot.addMouseListener(new MouseAdapter() {
 			@Override
@@ -126,27 +118,32 @@ public class LoginWindow {
 		gbc_btnForgot.gridx = 1;
 		gbc_btnForgot.gridy = 4;
 		frmLogin.getContentPane().add(btnForgot, gbc_btnForgot);
-		
+
 		btnLogIn = new JButton("Log In");
 		btnLogIn.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				client.sendMessage("login", loginInputField.getText(), String.valueOf(passwordInputField.getPassword()));
-				String answer = null;
+				String loginToSend = loginInputField.getText();
+				String pinToSend = String.valueOf(passwordInputField.getPassword());
+				client.sendMessage("login", loginToSend, pinToSend);
+				String[] answer = null;
 				try {
-					 answer = client.getMessage();
+					 answer = client.getArrayFromMessage();
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				final JDialog dialog = new JDialog();
-				dialog.setAlwaysOnTop(true);
-				JOptionPane.showMessageDialog(dialog, answer, "Answer", JOptionPane.INFORMATION_MESSAGE);
-				loginInputField.setText("");
-				passwordInputField.setText("");
-				MainWindow window = new MainWindow();
-				window.frmNagrabank.setVisible(true);
-				frmLogin.setVisible(false);
+				if (answer[0].equals("success")) {
+					MainWindow window = new MainWindow();
+					window.frmNagrabank.setVisible(true);
+					frmLogin.setVisible(false);
+				} else {
+					final JDialog dialog = new JDialog();
+					dialog.setAlwaysOnTop(true);
+					JOptionPane.showMessageDialog(dialog, answer[1], "error while loggining in", JOptionPane.ERROR_MESSAGE);
+					loginInputField.setText("");
+					passwordInputField.setText("");
+				}
 			}
 		});
 		GridBagConstraints gbc_btnLogIn = new GridBagConstraints();
@@ -155,7 +152,7 @@ public class LoginWindow {
 		gbc_btnLogIn.gridx = 0;
 		gbc_btnLogIn.gridy = 5;
 		frmLogin.getContentPane().add(btnLogIn, gbc_btnLogIn);
-		
+
 		btnRegistration = new JButton("Registration");
 		btnRegistration.addMouseListener(new MouseAdapter() {
 			@Override
