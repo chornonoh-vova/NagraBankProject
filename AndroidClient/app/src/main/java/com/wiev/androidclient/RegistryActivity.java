25eda866c08ac873f0f79918c91c14d1d3a53c6b
Kondratenko Alexander <Lurcher13@github.com>
2017-11-07 11:54:25 +0200

    fdsvf
    faefre

--- AndroidClient/app/src/main/java/com/wiev/androidclient/RegistryActivity.java
+++ AndroidClient/app/src/main/java/com/wiev/androidclient/RegistryActivity.java
@@ -3,12 +3,14 @@ package com.wiev.androidclient;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-/**
- * Created by Марина on 01.11.2017.
- */
 
 public class RegistryActivity extends AppCompatActivity{
+    private Button registration = null;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
