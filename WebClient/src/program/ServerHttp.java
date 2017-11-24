5e0cfb84643c80a4ee6d70f68ef5d8e63406eebe
Владимир Черноног <gyilfr246@gmail.com>
2017-11-24 13:22:10 +0200

    js update

--- WebClient/src/program/ServerHttp.java
+++ WebClient/src/program/ServerHttp.java
@@ -14,6 +14,7 @@ public class ServerHttp {
     HttpServer server = HttpServer.create(new InetSocketAddress(80), 10);
     System.out.println("Server started, type to connect in browser: " + "http://"
         + Inet4Address.getLocalHost().getHostAddress() + "/login");
+    server.createContext("/registry", new LoadPage());
     server.createContext("/login", new LoadPage());
     server.createContext("/server", new WebHandler(db));
     server.createContext("/js/client.js", new LoadFile());
@@ -21,6 +22,8 @@ public class ServerHttp {
     server.createContext("/js/util.js", new LoadFile());
     server.createContext("/js/jquery-3.2.1.min.js", new LoadFile());
     server.createContext("/js/login.js", new LoadFile());
+    server.createContext("/js/registry.js", new LoadFile());
+    server.createContext("/styles.css", new LoadFile());
     server.start();
   }
 
