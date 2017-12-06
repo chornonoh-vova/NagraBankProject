fa4a976b6d7720c8e92ee9edb449c853a8df244e
Владимир Черноног <gyilfr246@gmail.com>
2017-12-06 18:09:44 +0200

    add filterFloat

--- WebClient/site/js/util.js
+++ WebClient/site/js/util.js
@@ -1,22 +1,27 @@
+/*function to check login*/
 function verifyLogin(login) {
 	let regExp = /^[a-zA-Z]+([/._]?[a-zA-Z0-9]+)*$/;
 	return regExp.test(login);
 }
 
+/*function to check pin code*/
 function verifyPinCode(pin) {
 	let regExp = /^[0-9]{4}$/;
 	return regExp.test(pin);
 }
 
+/*function to check if date is in format: YYYY-MM-DD*/
 function verifyDate(date) {
 	let regExp = /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/;
 	return regExp.test(date);
 }
 
+/*generate md5 hash from given input*/
 function getMd5Hash(input) {
 	return CryptoJS.MD5(input).toString();
 }
 
+/*function to set cookie with value and options like expires,path*/
 function setCookie(name, value, options) {
   options = options || {};
 
@@ -46,15 +51,26 @@ function setCookie(name, value, options) {
   document.cookie = updatedCookie;
 }
 
+/*function to get cookie value with given name*/
 function getCookie(name) {
   var matches = document.cookie.match(new RegExp(
     "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-  ));
+    ));
   return matches ? decodeURIComponent(matches[1]) : undefined;
 }
 
+/*deletes cookie with given name*/
 function deleteCookie(name) {
   setCookie(name, "", {
     expires: -1
   })
+}
+ 
+/*function to test if value is valid float*/
+function filterFloat(value) {
+  if(/^(\-|\+)?([0-9]+(\.[0-9]+)?)$/.test(value)) {
+    return Number(value);
+  } else {
+    return undefined;
+  }
 }
