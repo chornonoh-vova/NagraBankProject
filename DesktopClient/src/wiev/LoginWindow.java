4f43295dee1f009f95cb7b79e36c4252a91cd2b9
Владимир Черноног <gyilfr246@gmail.com>
2017-12-08 11:31:07 +0200

    add getQuestion method

--- DesktopClient/src/wiev/LoginWindow.java
+++ DesktopClient/src/wiev/LoginWindow.java
@@ -68,10 +68,6 @@ public class LoginWindow implements ShowMessage, Md5Hasher {
 	 */
 	public LoginWindow() {
 		initialize();
-		if (client.isClosed()) {
-		  showErrorMessage("Connection", "Connection with server suddenly closed");
-		  System.exit(1);
-		}
 	}
 
 	/**
