24a941e522be8d2ecd5886bbe0dcfdee3321b956
BBCmisha <32327745+BBCmisha@users.noreply.github.com>
2017-12-12 11:30:03 +0200

    debug forgot
    db

--- WebClient/site/js/forgot.js
+++ WebClient/site/js/forgot.js
@@ -37,23 +37,23 @@ $("document").ready(function () {
 				}
 			   }
 		});
-
-	$("#confirm").click(function () {
-		let loginToSend = $("#userLogin").val();
-		let firstPin = getMd5Hash($("#pin").val());
-		let secondPin = getMd5Hash($("#confirm_pin").val());
-			if ($("#pin").val() !== $("#confirm_pin").val()) {
-				alert("Not equal pins!!!");
-				return;
-			}
-
-				client.openConnection();
-				client.sendMessage("changePin", loginToSend, firstPin);
-				let answer2 = client.getArrayFromMessage();
-				if(answer2[0] === "success") {
-					alert("Pin changed");
+	if (answer1[0] === "success") {
+		$("#confirm").click(function () {
+			let loginToSend = $("#userLogin").val();
+			let firstPin = getMd5Hash($("#pin").val());
+			let secondPin = getMd5Hash($("#confirm_pin").val());
+				if ($("#pin").val() !== $("#confirm_pin").val()) {
+					alert("Not equal pins!!!");
+					return;
 				}
 
-	});
+					client.openConnection();
+					client.sendMessage("changePin", loginToSend, firstPin);
+					let answer2 = client.getArrayFromMessage();
+					if(answer2[0] === "success") {
+						alert("Pin changed");
+					}
 
+		});
+	}
 	});
