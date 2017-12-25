9ca99658b0665329f3d4ca007900b17f2533e6fb
hbvhuwe <chernonog.vova@gmail.com>
2017-12-25 23:32:48 +0200

    update

--- Server/src/program/Program.java
+++ Server/src/program/Program.java
@@ -1,7 +1,9 @@
 package program;
 
 import java.net.InetAddress;
+import java.net.NetworkInterface;
 import java.net.ServerSocket;
+import java.util.Enumeration;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -16,6 +18,7 @@ import server.MultiServerThread;
  */
 public class Program {
   static ExecutorService executor = Executors.newFixedThreadPool(10);
+  private static String address;
 
   public static void main(String[] args) {
     int portNumber = 4444;
@@ -31,8 +34,11 @@ public class Program {
     }, "db-close"));
 
     try (ServerSocket serverSocket = new ServerSocket(portNumber)) {
-
-      System.out.println("Server started at ip: " + InetAddress.getLocalHost().getHostAddress());
+      Enumeration<InetAddress> e = NetworkInterface.getByName("wlp3s0").getInetAddresses();
+      while (e.hasMoreElements()) {
+      	address = e.nextElement().getHostAddress();
+      }
+      System.out.println("Server started at ip: " + address);
       System.out.println("Press Ctrl+C to close");
 
       while (!serverSocket.isClosed()) {
--- Server/users-table.sql
+++ Server/users-table.sql
@@ -1,2 +1,12 @@
-insert into users values ('2','1000','1111','1998-10-10','test1','1','1','1');
-select * From users;
+CREATE TABLE `users` (
+  `user_id` int(16) NOT NULL,
+  `balance` double DEFAULT NULL,
+  `pin` varchar(45) NOT NULL,
+  `birthdate` date NOT NULL,
+  `user_login` varchar(45) NOT NULL,
+  `secret_question` varchar(45) NOT NULL,
+  `secret_answer` varchar(45) NOT NULL,
+  `status` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `user_login_UNIQUE` (`user_login`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Users of the bank'
