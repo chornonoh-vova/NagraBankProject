f50a5a084c46632c762dcd10aa86df9202fdd699
BBCmisha <32327745+BBCmisha@users.noreply.github.com>
2017-12-28 13:33:45 +0200

    fix braces

--- WebClient/site/js/forgot.js
+++ WebClient/site/js/forgot.js
@@ -1,8 +1,8 @@
 $("document").ready(function () {
 	let client = new Client();
 	$("#userLogin").keypress(function(e){
-	   if(e.keyCode==13){
-		   let loginToSend = $("#userLogin").val();
+		if(e.keyCode==13){
+			let loginToSend = $("#userLogin").val();
 			if (!verifyLogin(loginToSend)) {
 				alert("Wrong login or pin-code");
 				return;
@@ -17,33 +17,33 @@ $("document").ready(function () {
 			} else if(answer[0] === "error") {
 				alert("Wrong login");
 			}
-		   }
+		}
 	});
 
 	$("#answer").keypress(function(e){
-		   if(e.keyCode==13){
-		   	let loginToSend = $("#userLogin").val();
-			   let answerForLogin = $("#answer").val();
-
-				client.openConnection();
-				client.sendMessage("checkQuestion", loginToSend, answerForLogin);
-
-				let answer1 = client.getArrayFromMessage();
-				if (answer1[0] === "error") {
-					alert("Wrong answer!");
-					$("#confirm").click(function () {
-						alert("Your answer wrong!");
-						});
-				} else if (answer1[0] === "success") {
-					alert("Answer is correct!");
-					$("#confirm").click(function () {
+		if(e.keyCode==13){
 			let loginToSend = $("#userLogin").val();
-			let firstPin = getMd5Hash($("#pin").val());
-			let secondPin = getMd5Hash($("#confirm_pin").val());
-				if ($("#pin").val() !== $("#confirm_pin").val()) {
-					alert("Not equal pins!!!");
-					return;
-				}
+			let answerForLogin = $("#answer").val();
+
+			client.openConnection();
+			client.sendMessage("checkQuestion", loginToSend, answerForLogin);
+
+			let answer1 = client.getArrayFromMessage();
+			if (answer1[0] === "error") {
+				alert("Wrong answer!");
+				$("#confirm").click(function () {
+					alert("Your answer wrong!");
+				});
+			} else if (answer1[0] === "success") {
+				alert("Answer is correct!");
+				$("#confirm").click(function () {
+					let loginToSend = $("#userLogin").val();
+					let firstPin = getMd5Hash($("#pin").val());
+					let secondPin = getMd5Hash($("#confirm_pin").val());
+					if ($("#pin").val() !== $("#confirm_pin").val()) {
+						alert("Not equal pins!!!");
+						return;
+					}
 
 					client.openConnection();
 					client.sendMessage("changePin", loginToSend, firstPin);
@@ -52,8 +52,8 @@ $("document").ready(function () {
 						alert("Pin changed");
 					}
 
-		});
-				}
-			   }
-
-		});
+				});
+			}
+		}
+	});
+});
